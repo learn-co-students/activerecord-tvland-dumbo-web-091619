689a0ed59b7c395e390133d0fbd7d29f1e81a420
github username <github email address>
2019-09-27 08:20:29 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters_roles.map do |character_role|
+      "#{character_role[0]} - #{character_role[1]}"
+    end
+  end
+
+  private 
+  def characters_roles
+    character_names = self.characters.map(&:name)
+    show_names = self.shows.map(&:name)
+    character_names.zip(show_names)
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,18 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(args)
+    self.show = Show.new(args)
+    self
+  end
+
+  def build_network(args)
+    self.show.network = Network.new(args)
+  end  
+
 end
