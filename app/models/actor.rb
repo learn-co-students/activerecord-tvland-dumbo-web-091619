ab114f04d6657d404382b874fdb69f1ba0718423
Reina <r.clarkemitchell@gmail.com>
2019-10-04 15:21:14 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+has_many :characters
+has_many :shows, through: :characters
+
+    def full_name
+        fullN = "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        arr = []
+        self.characters.map {|show| "#{show.name} - #{Show.find(show_ids)[0].name}" }
+    end
+    
+
 end
